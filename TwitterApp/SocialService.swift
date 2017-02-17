50e13a01ccaf7a08c82dd7047f8eb106d06ed5bf
AJ Miller <aj@knockmedia.com>
2017-02-16 21:45:00 -0500

    Stub out message struct for our social network data format Add protocol method to transform social network data into that struct format

--- TwitterApp/SocialService.swift
+++ TwitterApp/SocialService.swift
@@ -9,8 +9,19 @@
 import Foundation
 
 /**
+*  This struct should be the embodiment of all the data we use in each message
+*  Each social network's data will need to be parsed to adhere to this structure
+*/
+struct SocialNetworkMessage {
+	var author: String?
+	var message: String
+	var image: URL?
+}
+
+/**
 *  This protocol should be adapted for any new social networks
 */
 protocol SocialNetworkService {
-	func getMostRecentMessages()
+	func getMostRecentMessages(dataCallback: @escaping (_ messages: [SocialNetworkMessage]?, _ error: Error?) -> ())
+	func transformJSONIntoMessages(jsonData: Any?) -> [SocialNetworkMessage]?
 }
